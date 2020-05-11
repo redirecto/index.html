0e99b8d86e62106f30c354c5f7bd61f9ac1c2cad
serverss <64730395+serverss@users.noreply.github.com>
2020-05-11 19:25:38 +0200

    Update and rename index.js to url.html

--- index.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-var url = require("url");
-
-http.createServer(function(req, res) {
-  var pathname = url.parse(req.url).pathname;
-  res.writeHead(301,{Location: 'http://new-website.com/' + pathname});
-  res.end();
-}).listen(8888);
